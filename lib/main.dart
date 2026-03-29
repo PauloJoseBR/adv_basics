1cde82ddb0b204f204e141a81d9c1b01a21cac53
Paulo <paulotec96@gmail.com>
2026-03-28 23:22:44 -0300

    Refactor main entry point to use Container with gradient background and add launch configuration for VSCode

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,20 @@ import 'package:adv_basics/start_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Scaffold(body: StartScreen())));
+  runApp(
+    MaterialApp(
+      home: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              const Color.fromARGB(255, 42, 1, 40),
+              const Color.fromARGB(255, 79, 1, 93),
+            ],
+          ),
+        ),
+
+        child: const StartScreen(),
+      ),
+    ),
+  );
 }
