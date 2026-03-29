19384c9a67f442b877ec8df85d2cb8cf3a882702
Paulo <paulotec96@gmail.com>
2026-03-29 00:22:31 -0300

    Refactor code structure for improved readability and maintainability

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -5,6 +5,33 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: const Text('Start Screen!!'));
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/images/quiz-logo.png', width: 300),
+          SizedBox(height: 80),
+          Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+              side: BorderSide(color: Colors.white),
+              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+              textStyle: const TextStyle(fontSize: 25),
+            ),
+            child: Text('Start Quiz!'),
+          ),
+        ],
+      ),
+    );
   }
 }
