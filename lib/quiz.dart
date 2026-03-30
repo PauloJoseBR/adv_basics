45285b44560221396c4a91f8430b332bcf0e9cf9
Paulo <paulotec96@gmail.com>
2026-03-30 11:24:53 -0300

    Implement Quiz screen and update main entry point to launch it

---
+++ lib/quiz.dart
@@ -0,0 +1,35 @@
+import 'package:adv_basics/start_screen.dart';
+import 'package:flutter/material.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 2, 113, 119),
+                Color.fromARGB(255, 47, 130, 169),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+
+          child: const StartScreen(),
+        ),
+      ),
+    );
+  }
+}
