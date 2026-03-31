d7b49848c39fbf9a38435508277370d5968cef6c
Paulo <paulotec96@gmail.com>
2026-03-31 15:40:16 -0300

    Update QuestionsScreen to handle dynamic question navigation and answer selection

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -12,9 +12,17 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(context) {
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[currentQuestionIndex];
 
     return SizedBox(
       width: double.infinity,
@@ -30,12 +38,8 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
               textAlign: TextAlign.center,
             ),
             ...currentQuestion.getShuffledQuestions().map(
-              (answer) => AnswerButton(
-                answerText: answer,
-                onTap: () {
-                  return;
-                },
-              ),
+              (answer) =>
+                  AnswerButton(answerText: answer, onTap: answerQuestion),
             ),
           ],
         ),
