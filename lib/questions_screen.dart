8c5aaebb00a90d6a963642a3e6a8513932647430
Paulo <paulotec96@gmail.com>
2026-03-30 22:38:24 -0300

    Update QuestionsScreen to display dynamic question text and answers

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import 'answer_button.dart';
+import 'data/questions_data.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -13,18 +14,20 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
+
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'The question...',
-            style: TextStyle(color: Colors.white, fontSize: 35),
+          Text(
+            currentQuestion.text,
+            style: TextStyle(color: Colors.white, fontSize: 20),
           ),
-          AnswerButton(answerText: 'Answer Text... ', onTap: () {}),
-          AnswerButton(answerText: 'Answer Text... ', onTap: () {}),
-          AnswerButton(answerText: 'Answer Text... ', onTap: () {}),
+          AnswerButton(answerText: currentQuestion.answers[0], onTap: () {}),
+          AnswerButton(answerText: currentQuestion.answers[1], onTap: () {}),
+          AnswerButton(answerText: currentQuestion.answers[2], onTap: () {}),
         ],
       ),
     );
